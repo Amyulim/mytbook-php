e8183b7a8c3e44e76c96e7fb65631ad4ba926bb4
amyulim <apppe77@gmail.com>
2019-01-30 21:39:28 -0800

    php

--- select_user.php
+++ select_user.php
@@ -4,8 +4,7 @@ header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
  try {
-    $conn = new PDO("mysql:host=localhost;dbname=mytbook","root", "root");
-    echo "Connected &nbsp; ";
+    $conn = new PDO("mysql:host=k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=hxx7ktin4x3q7iue","caqwo1iz5lww951z", "zqigtmtt81za05j2");
   }catch (PDOExpection $e){
     echo "Error" .$e->getMessage();
   }
@@ -14,21 +13,22 @@ $user_email = $_POST['user_email'];
 $student_id = $_POST['student_id'];
 $user_pass = $_POST['user_pass'];
 
-$query = "SELECT user_id FROM users WHERE user_email='$user_email' AND user_pass='$user_pass'";
+//$query = "SELECT id FROM users WHERE user_email='$user_email' AND user_pass='$user_pass'";
+$query = "SELECT * FROM users";
 
 $result = $conn->query($query);
 if($result){
   $users = $result->fetchAll();
-  if(!empty($users)){
-    echo json_encode(array(
-      "status"=>true,
-      "id"=>$users[0]["id"]
-    ));
-  } else {
-    echo json_encode(false);
-  }
+//  if(!empty($users)){
+//    echo json_encode(array(
+//      "status"=>true,
+//      "id"=>$users[0]["id"]
+//    ));
+//  } else {
+//    echo json_encode(false);
+//  }
 } else {
   echo json_encode(false);
-}
+} 
 
 ?>
