548fb3030e3e8edb9c3a08f334436c147f1158ad
amyulim <apppe77@gmail.com>
2019-01-30 23:55:39 -0800

    .

--- select_book.php
+++ select_book.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
 try {
     $conn = new PDO("mysql:host=k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=hxx7ktin4x3q7iue", "caqwo1iz5lww951z", "zqigtmtt81za05j2");
-    echo "Good";
+//    echo "Good";
 }
 catch (PDOExpection $e) {
     echo "Error" . $e->getMessage();
@@ -15,7 +15,7 @@ $query = "SELECT * FROM books";
 
 $result = $conn->query($query);
 
-echo "book_title";
+
 if($result){
   $books = $result->fetchAll();
   if(!empty($books)){
