9f1efb64902bf64f856c3ec0f1e8960e9f08ff3d
amyulim <apppe77@gmail.com>
2019-01-30 23:08:07 -0800

    Update select_book.php

--- select_book.php
+++ select_book.php
@@ -3,18 +3,14 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
- try {
-    $conn = new PDO("mysql:host=k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=hxx7ktin4x3q7iue","caqwo1iz5lww951z", "zqigtmtt81za05j2");
-   echo "Good";
-  }catch (PDOExpection $e){
-    echo "Error" .$e->getMessage();
-  }
+try {
+    $conn = new PDO("mysql:host=k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=hxx7ktin4x3q7iue", "caqwo1iz5lww951z", "zqigtmtt81za05j2");
+    echo "Good";
+}
+catch (PDOExpection $e) {
+    echo "Error" . $e->getMessage();
+}
 
-$user_email = $_POST['user_email'];
-$student_id = $_POST['student_id'];
-$user_pass = $_POST['user_pass'];
-
-//$query = "SELECT id FROM users WHERE user_email='$user_email' AND user_pass='$user_pass'";
 $query = "SELECT * FROM books";
 
 $result = $conn->query($query);
