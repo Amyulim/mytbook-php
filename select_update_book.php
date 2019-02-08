108d855c22fef936ece77d8baa5ba0bb662d99f0
Eveyen <eveyenbcit@gmail.com>
2019-02-08 14:11:32 -0800

    Update select_update_book.php

--- select_update_book.php
+++ select_update_book.php
@@ -5,29 +5,26 @@ header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
 try {
     $conn = new PDO("mysql:host=k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=hxx7ktin4x3q7iue", "caqwo1iz5lww951z", "zqigtmtt81za05j2");
-    echo "dd"
+    echo "Good";
 }
 catch (PDOExpection $e) {
     echo "Error" . $e->getMessage();
 }
 
+$query = "SELECT * FROM books";
 
-//$query = "SELECT * FROM books ";
-//
-//
-//
-//$result = $conn->query($query);
-//
-//if($result){
-//  $books = $result->fetchAll();
-//  if(!empty($users)){
-//    echo json_encode($books);
-//  } else {
-//    echo json_encode(false);
-//  }
-//} else {
-//  echo json_encode(false);
-//} 
+$result = $conn->query($query);
 
+
+if($result){
+  $books = $result->fetchAll();
+  if(!empty($books)){
+    echo json_encode($books);
+  } else {
+    echo json_encode(false);
+  }
+} else {
+  echo json_encode(false);
+} 
 
 ?>
