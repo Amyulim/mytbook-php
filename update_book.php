2c306735ad301abd56f70fb57f860f4fb9ab50f2
Eveyen <eveyenbcit@gmail.com>
2019-02-10 17:55:10 -0800

    update book

--- update_book.php
+++ update_book.php
@@ -30,7 +30,7 @@ $book_mdate = $_POST['book_mdate'];
 
 $query = "UPDATE books SET book_title='{$_POST["book_title"]}' , book_isbn='{$_POST["book_isbn"]}', book_course='{$_POST["book_course"]}', book_price='{$_POST["book_price"]}', book_condition='{$_POST["book_condition"]}', book_desc='{$_POST["book_desc"]}', book_img='{$_POST["book_img"]}', book_mdate='{$_POST["book_mdate"]}' WHERE user_id='{$_POST["user_id"]}' AND book_id='{$_POST["book_id"]}' " ;
 
-//echo $query;
+echo $query;
 
 $result = $conn->query($query);
 
