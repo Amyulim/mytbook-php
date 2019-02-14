c067be0e42c96b05fe8f27a992052cc96327ca8d
Eveyen <eveyenbcit@gmail.com>
2019-02-14 13:00:52 -0800

    delete book

--- delete_book.php
+++ delete_book.php
@@ -13,17 +13,9 @@ catch (PDOExpection $e) {
 
 $user_id = $_POST['user_id'];
 $book_id = $_POST['book_id'];
-$book_title = $_POST['book_title'];
-$book_isbn = $_POST['book_isbn'];
-$book_course = $_POST['book_course'];
-$book_price = $_POST['book_price'];
-$book_condition = $_POST['book_condition'];
-$book_desc = $_POST['book_desc'];
-$book_img = $_POST['book_img'];
-$book_mdate = $_POST['book_mdate'];
 
 
-$query = "UPDATE books SET book_title='{$_POST["book_title"]}' , book_isbn='{$_POST["book_isbn"]}', book_course='{$_POST["book_course"]}', book_price='{$_POST["book_price"]}', book_condition='{$_POST["book_condition"]}', book_desc='{$_POST["book_desc"]}', book_img='{$_POST["book_img"]}', book_mdate='{$_POST["book_mdate"]}' WHERE user_id='{$_POST["user_id"]}' AND book_id='{$_POST["book_id"]}' " ;
+$query = "DELETE FROM books WHERE user_id='{$_POST["user_id"]}' AND book_id='{$_POST["book_id"]}' " ;
 
 //echo $query;
 
