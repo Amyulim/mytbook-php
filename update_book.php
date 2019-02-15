150d33fdea9a83c140af5e749edb1344f72bcc82
amyulim <apppe77@gmail.com>
2019-02-14 22:47:17 -0800

    Update update_book.php

--- update_book.php
+++ update_book.php
@@ -23,7 +23,7 @@ $book_img = $_POST['book_img'];
 $book_mdate = $_POST['book_mdate'];
 
 
-$query = "UPDATE books SET book_title='{$_POST["book_title"]}' , book_isbn='{$_POST["book_isbn"]}', book_course='{$_POST["book_course"]}', book_price='{$_POST["book_price"]}', book_condition='{$_POST["book_condition"]}', book_desc='{$_POST["book_desc"]}', book_img='{$_POST["book_img"]}', book_mdate='{$_POST["book_mdate"]}' WHERE user_id='{$_POST["user_id"]}' AND book_id='{$_POST["book_id"]}' " ;
+$query = "UPDATE books SET book_title='{$_POST["book_title"]}' , book_isbn='{$_POST["book_isbn"]}', book_course='{$_POST["book_course"]}', book_price='{$_POST["book_price"]}', book_condition='{$_POST["book_condition"]}', book_desc='{$_POST["book_desc"]}', book_img='{$_POST["book_img"]}', book_mdate='{$_POST["book_mdate"]}', book_stats='{$_POST["book_status"]}' WHERE user_id='{$_POST["user_id"]}' AND book_id='{$_POST["book_id"]}' " ;
 
 //echo $query;
 
